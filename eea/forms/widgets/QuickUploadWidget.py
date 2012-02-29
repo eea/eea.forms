aad290c2c7204280e230790fad1d7685f8782534
voineali <voineali@24821446-e7ea-0310-81ac-b189f4507b0b>
2012-02-29 16:49:42 +0000

    Refs #4965   - QuickUploadWidget - Added possibility to restrict uploaded files to given file extensions and to force portal_type for new uploaded objects.

--- eea/forms/widgets/QuickUploadWidget.py
+++ eea/forms/widgets/QuickUploadWidget.py
@@ -8,4 +8,11 @@ class QuickUploadWidget(TypesWidget):
     _properties = TypesWidget._properties.copy()
     _properties.update({
         'macro' : "quick_upload_widget",
+
+        # Allowed file extensions
+        'mediaupload' : '*.txt; *.csv; *.tsv; *.tab',
+
+        # Portal type to use on object creation
+        'typeupload': 'File',
+
     })
